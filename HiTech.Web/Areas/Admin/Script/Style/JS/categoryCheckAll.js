535f9c2e12c3e9882e0761303020aa43531beb55
MINHTAM\MinhTam <tamminh.2910@gmail.com>
2018-11-20 14:11:57 +0700

    HiTech is a website

---
+++ HiTech.Web/Areas/Admin/Script/Style/JS/categoryCheckAll.js
@@ -0,0 +1,12 @@
+﻿$(document).ready(function () {
+   
+    $('#CheckboxAll').click(function () {
+        if ($(this).is(":checked")) {
+            $('.CheckboxID').prop("checked", true)
+        }
+        else {
+            $('.CheckboxID').prop("checked", false)
+        }
+
+    })
+})
